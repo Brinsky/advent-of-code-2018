77d792dd736d1cfbf8c7461e43257ab5791487aa
Brian Stottler <brianstottler@gmail.com>
2018-12-22 13:29:50 -0500

    Optimize factor-summing logic

--- src/advent/Day19.java
+++ src/advent/Day19.java
@@ -103,10 +103,16 @@ public class Day19 {
 
   private static long sumFactors(int number) {
     int sum = 0;
-    for (int i = 1; i <= number; i++) {
+    int i;
+    for (i = 1; i * i < number; i++) {
       if (number % i == 0) {
         sum += i;
+        sum += number / i;
       }
+    }
+    // Check if square root is a factor
+    if (number % i == 0) {
+      sum += i;
     }
     return sum;
   }
